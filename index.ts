78f3d517dbeaefa6439d102408eb91b4b7243775
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-02-01 23:37:02 +0600

    Multer commit

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ import cors from 'cors';
 const app = express();
 const port = 8000;
 
+
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 
--- routers/products.ts
+++ routers/products.ts
@@ -2,6 +2,7 @@ import crypto from 'crypto';
 import {Router} from 'express';
 import {Product, ProductWithoutId} from "../types";
 import fileDb from "../fileDb";
+import {imagesUpload} from "../multer";
 const productsRouter = Router();
 productsRouter.get('/', async (req, res)=>{
     const products = await fileDb.getItems();
@@ -14,12 +15,13 @@ productsRouter.get('/:id', async (req, res)=>{
     res.send(product);
 });
 
-productsRouter.post('/', async(req, res)=>{
+productsRouter.post('/', imagesUpload.single('image'), async(req, res)=>{
 
     const product: ProductWithoutId = {
         title: req.body.title,
-        price: req.body.price,
+        price: parseFloat(req.body.price),
         description: req.body.description,
+        image: req.file ? req.file.filename : null,
     };
 
     const newProduct = await fileDb.addItem(product);
